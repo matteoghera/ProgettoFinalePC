5f2877309b212834adaa93d75cfc756c729cbbcd
Matteo Ghera <matteo.ghera@stud.unifi.it>
2019-06-28 08:27:20 +0200

    re-imposto test

--- AWS_test/runTest2.sh
+++ AWS_test/runTest2.sh
@@ -5,35 +5,35 @@
 #Dimensione file 2M
 for i in $(seq 1 9);
 do
-java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.SequentialBloomFilterApp 1 1050 data/sample100K.txt data/people100K.txt
+java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.SequentialBloomFilterApp 1 100 data/sample10K.txt data/people10K.txt
 done
 
 #2 Threads
 for i in $(seq 1 9);
 do
-java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 2 1050 data/sample100K.txt data/people100K.txt
+java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 2 100 data/sample10K.txt data/people10K.txt
 done
 
 #3 Threads
 for i in $(seq 1 9);
 do
-java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 3 1050 data/sample100K.txt data/people100K.txt
+java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 3 100 data/sample10K.txt data/people10K.txt
 done
 
 #5 Threads
 for i in $(seq 1 9);
 do
-java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 5 1050 data/sample100K.txt data/people100K.txt
+java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 5 100 data/sample10K.txt data/people10K.txt
 done
 
 #10 Threads
 for i in $(seq 1 9);
 do
-java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 10 1050 data/sample100K.txt data/people100K.txt
+java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 10 100 data/sample10K.txt data/people10K.txt
 done
 
 #15 Threads
 for i in $(seq 1 9);
 do
-java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 15 1050 data/sample100K.txt data/people100K.txt
+java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 15 100 data/sample10K.txt data/people10K.txt
 done
