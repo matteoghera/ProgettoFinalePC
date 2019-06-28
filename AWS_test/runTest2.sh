59c6268a5aa20a01470df3b7ad2d70601a4deb1a
Matteo Ghera <matteo.ghera@stud.unifi.it>
2019-06-28 08:09:44 +0200

    imposto dataset per analisi performance al variare dei thread

--- AWS_test/runTest2.sh
+++ AWS_test/runTest2.sh
@@ -5,35 +5,35 @@
 #Dimensione file 2M
 for i in $(seq 1 9);
 do
-java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.SequentialBloomFilterApp 1 20560 data/sample2M.txt data/people2M.txt
+java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.SequentialBloomFilterApp 1 1050 data/sample100K.txt data/people100K.txt
 done
 
 #2 Threads
 for i in $(seq 1 9);
 do
-java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 2 20560 data/sample2M.txt data/people2M.txt
+java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 2 1050 data/sample100K.txt data/people100K.txt
 done
 
 #3 Threads
 for i in $(seq 1 9);
 do
-java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 3 20560 data/sample2M.txt data/people2M.txt
+java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 3 1050 data/sample100K.txt data/people100K.txt
 done
 
 #5 Threads
 for i in $(seq 1 9);
 do
-java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 5 20560 data/sample2M.txt data/people2M.txt
+java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 5 1050 data/sample100K.txt data/people100K.txt
 done
 
 #10 Threads
 for i in $(seq 1 9);
 do
-java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 10 20560 data/sample2M.txt data/people2M.txt
+java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 10 1050 data/sample100K.txt data/people100K.txt
 done
 
 #15 Threads
 for i in $(seq 1 9);
 do
-java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 15 20560 data/sample2M.txt data/people2M.txt
+java -cp BloomFilter.jar com.unifi.pc.gc.bloom.app.ParallelBloomFilterApp 15 1050 data/sample100K.txt data/people100K.txt
 done
